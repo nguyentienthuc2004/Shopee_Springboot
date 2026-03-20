6a8ff43a5f3f8687394d6690c8c4a26aa25161e3
st12z <ckp2004vn@gmail.com>
2026-03-20 17:26:52 +0700

    Cấu hình swagger

--- src/main/java/org/thuc/shoppe/controller/ProductController.java
+++ src/main/java/org/thuc/shoppe/controller/ProductController.java
@@ -1,10 +1,8 @@
 package org.thuc.shoppe.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.thuc.shoppe.model.dto.PageResponseDto;
