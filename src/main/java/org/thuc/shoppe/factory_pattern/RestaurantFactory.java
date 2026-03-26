89bc6afe5ccf9d00a3ac28ad5307e223d506b78e
st12z <ckp2004vn@gmail.com>
2026-03-26 15:00:19 +0700

    dùng bean, map factory pattern

--- src/main/java/org/thuc/shoppe/factory_pattern/RestaurantFactory.java
+++ src/main/java/org/thuc/shoppe/factory_pattern/RestaurantFactory.java
@@ -1,15 +1,19 @@
 package org.thuc.shoppe.factory_pattern;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
+@RequiredArgsConstructor
 public class RestaurantFactory {
+    private final Map<String, Restaurant> restaurantMap;
     public Restaurant getRestaurant(String type){
-        if(type.equalsIgnoreCase("beef")){
-            return new BeefRestaurant();
-        } else if(type.equalsIgnoreCase("veggie")){
-            return new VeggieRestaurant();
-        }
-        return null;
+       Restaurant restaurant = restaurantMap.get(type);
+         if(restaurant == null){
+             throw new UnsupportedOperationException("Restaurant type not supported: " + type);
+         }
+         return restaurant;
     }
 }
--- src/main/java/org/thuc/shoppe/factory_pattern/VeggieRestaurant.java
+++ src/main/java/org/thuc/shoppe/factory_pattern/VeggieRestaurant.java
@@ -1,5 +1,8 @@
 package org.thuc.shoppe.factory_pattern;
 
+import org.springframework.stereotype.Component;
+
+@Component("veggie")
 public class VeggieRestaurant extends Restaurant{
     @Override
     public Burger createBurger() {
